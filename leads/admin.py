2e1980aacaef8fde008bc74d79334a219378d9cd
Andrey <justscoundrel@yandex.ru>
2021-03-02 14:06:44 +0300

    Add lead time fields

--- leads/admin.py
+++ leads/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class LeadAdmin(admin.ModelAdmin):
+	list_display = ('name', 'date_born', 'status', 'date_update')
+
 admin.site.register(Status)
-admin.site.register(Lead)
+admin.site.register(Lead, LeadAdmin)
 
 
--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+from django.utils import timezone
 from django.db import models
 from ckeditor.fields import RichTextField
 
@@ -16,12 +18,19 @@ class Lead(models.Model):
 	name = models.CharField(max_length=100, blank=True, verbose_name='Имя')
 	email = models.EmailField(verbose_name='Почта', blank=True)
 	phone = models.CharField(max_length=15, blank=True, verbose_name='Телефон')
+	date_born = models.DateTimeField(auto_now_add=timezone.now(), verbose_name='Дата создания')
+	date_update = models.DateTimeField(auto_now_add=timezone.now(), verbose_name='Дата смены статуса')
 	status = models.ForeignKey(Status, default=1, on_delete=models.SET_NULL, null=True, verbose_name='Статус')
 	description = RichTextField(verbose_name='Описание', blank=True)
 
 	class Meta:
 		verbose_name = 'Лид'
 		verbose_name_plural = 'Лиды'
+
+	def save(self, *args, **kwargs):
+		''' On save, update date_update '''
+		self.date_update = timezone.now()
+		return super(Lead, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name + ' ' + self.status.title
