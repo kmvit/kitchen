e994ae989bb073c999bcedb45606b8dbc004eaa6
justscoundrel <justscoundrel@yandex.ru>
2021-03-18 18:31:40 +0300

    Add mail

--- kitchensite/settings.py
+++ kitchensite/settings.py
@@ -140,7 +140,7 @@ MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 CKEDITOR_CONFIGS = {
@@ -207,3 +207,9 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
--- leads/views.py
+++ leads/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse
 from .models import Lead
 from .forms import LeadForm
 from django.views import generic
+from django.core.mail import send_mail
 
 class LeadCreate(generic.CreateView):
 	template_name = 'lead_create.html'
 	form_class = LeadForm
+
+	def form_valid(self, form):
+		send_mail(
+			'Заявка с сайта',
+			'Зайдите в админку и проверьте лиды!',
+			'info@kmv-it.ru',
+			['manager@kmv-it.ru'],
+			fail_silently=False
+		)
+
+		return HttpResponseRedirect(self.get_success_url())
 
 	def get_success_url(self):
 		return reverse('lead:success')
