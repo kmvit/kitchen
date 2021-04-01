5ee148e26d59d8c77cc4716bacd073d7c9133b19
justscoundrel <justscoundrel@yandex.ru>
2021-04-01 22:43:13 +0300

    Add changes

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,6 +6,12 @@ class WorklistView(generic.ListView):
 	template_name = 'work_list.html'
 	queryset = Work.objects.all()
 
+	def get_context_data(self, **kwargs):
+		context = super(WorklistView, self).get_context_data(**kwargs)
+		context['my_title'] = 'Портфолио'
+		return context
+
+
 
 class WorkDetailView(generic.DetailView):
 	template_name = 'work_detail.html'
